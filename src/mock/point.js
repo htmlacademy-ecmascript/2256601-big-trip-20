2105334389bf134179ed5e8b4b038f7d9b32da1a
Николай Бабаркин <nikola_@bk.ru>
2023-05-21 12:48:02 +0300

    2.13 Шаблонизируй это

---
+++ src/mock/point.js
@@ -0,0 +1,18 @@
+import { getRandomInteger } from '../utils.js';
+import { Prise } from './const.js';
+import { getDate } from './unils';
+
+function generatePoint (type, destinationId, offerIds) {
+  return {
+    id: crypto.randomUUID(),
+    basePrice: getRandomInteger(Prise.MIN, Prise.MAX),
+    dateFrom: getDate({next: false}),
+    dateTo: getDate({next: true}),
+    destination: destinationId,
+    isFavourite: !!getRandomInteger(0,1),
+    offers: offerIds,
+    type
+  };
+}
+
+export {generatePoint};
